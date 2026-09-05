5d3dc8f359bfa47b1253d6d0354fd8cba62fad29
Peter Morgenstern <mail@petergundel.de>
2026-09-05 09:36:10 +0200

    Format existing SQL files

--- migrations/20250804092718_add_resolved_to_comments.up.sql
+++ migrations/20250804092718_add_resolved_to_comments.up.sql
@@ -2,7 +2,7 @@
 ALTER TABLE comments ADD COLUMN resolved BOOLEAN NOT NULL DEFAULT FALSE;
 
 -- Create index on resolved column for efficient queries
-CREATE INDEX idx_comments_resolved ON comments(resolved);
+CREATE INDEX idx_comments_resolved ON comments (resolved);
 
 -- Create composite index for efficient queries on review_id and resolved status
-CREATE INDEX idx_comments_review_resolved ON comments(review_id, resolved);
+CREATE INDEX idx_comments_review_resolved ON comments (review_id, resolved);
